b8684f649569fe3576435799f6a8ac1c22bdf96e
Ronison <ronisonmuriel@gmail.com>
2021-01-04 14:08:35 -0400

    Comunicação entre os componentes

---
+++ componentes/componente-intro/src/Contadores.vue
@@ -0,0 +1,20 @@
+<template>
+
+    <div class="contadores">
+        <app-contador v-for="c in 5" :key="c" />
+        <button>?</button>
+    </div>
+
+</template>
+
+<script>
+import Contador from './Contador.vue'
+export default {
+  components: { 'app-contador': Contador }
+
+}
+</script>
+
+<style>
+
+</style>
